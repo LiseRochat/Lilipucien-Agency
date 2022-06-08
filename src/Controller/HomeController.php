30da735400f17ea603be52cdc079129164efa320
Lise Rochat <liserochat@live.fr>
2022-06-08 22:25:09 +0200

    create templates and controllers

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Biens;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home')]
+    public function index(ManagerRegistry $doctrine): Response
     {
+        //$biens = $doctrine->getRepository(Biens::class)->findAll();
+        $biens = $doctrine->getRepository(Biens::class)->findBy([], ['id' => 'DESC']);
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'biens' => $biens 
         ]);
     }
 }
