a09d36f3a12be692cf6bd94058cd57ed88739e79
Lise Rochat <liserochat@live.fr>
2022-06-09 11:06:32 +0200

    clean use in produitPageController

--- src/Controller/ProductPageController.php
+++ src/Controller/ProductPageController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use DateTime;
 use App\Entity\Bien;
 use App\Form\BienType;
 use DateTimeImmutable;
@@ -10,10 +9,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductPageController extends AbstractController
