a21b1558c61089767e6d11eb45556317c3f63f0c
Lise Rochat <liserochat@live.fr>
2022-06-09 10:47:35 +0200

    add product on bdd with form

--- src/Controller/ProductPageController.php
+++ src/Controller/ProductPageController.php
@@ -6,14 +6,15 @@ use DateTime;
 use App\Entity\Bien;
 use App\Form\BienType;
 use DateTimeImmutable;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductPageController extends AbstractController
 {
@@ -29,7 +30,7 @@ class ProductPageController extends AbstractController
     }
 
     #[Route('/bien/add', name: 'add_product')]
-    public function add()
+    public function add(Request $request, ManagerRegistry $doctrine)
     {
         //etape 1 : à la création, on instancie une objet vide
         $bien = new Bien();
@@ -50,6 +51,19 @@ class ProductPageController extends AbstractController
 
         //METHODE 02 : création formulaire avec le composant formType
         $formBien = $this->createForm(BienType::class, $bien);
+        $formBien->handleRequest($request);
+
+        if($formBien->isSubmitted() && $formBien->isValid()){
+
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($bien);
+            $entityManager->flush();
+
+            // Ajouter un message pour le feedback, dans la variable de session 'flash'
+            $this->addFlash('success_add', 'Le bien a été ajouté !');
+
+            return $this->redirectToRoute('app_home');
+        }
 
         return $this->render('product_page/form-add.html.twig',['formBien' => $formBien->createView()]);
         // return $this->renderForm('bien/form-add.html.twig', ['formBien' => $formBien]);
