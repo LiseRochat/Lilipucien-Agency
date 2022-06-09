ca712224863fa7d7461cedabc61c36c638d8fe83
Lise Rochat <liserochat@live.fr>
2022-06-09 19:22:30 +0200

    create table with all status

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Bridge\Doctrine\ManagerRegistry;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Status;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StatusController extends AbstractController
 {
     #[Route('/status', name: 'app_status')]
     public function index(ManagerRegistry $doctrine): Response
     {
-        $status = $doctrine->getRepository(Status::class)->findAll();
+        $status =  $doctrine->getRepository(Status::class)->findAll();
         return $this->render('status/status.html.twig', [
             'status' => $status
         ]);
