9a096925aa8f7386af35420bb441278a163a9478
Lise Rochat <liserochat@live.fr>
2022-06-10 16:22:11 +0200

    save in bdd message

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,7 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, EntityManager $doctrine): Response
     {
         // Etape 01 : On ccrée le formulaire
         $contact = new Contact();
@@ -19,6 +20,16 @@ class ContactController extends AbstractController
 
         // Etape02 : On crée le formulaire
         $formContact = $this->createForm(ContactType::class, $contact);
+
+        // Etape03 : On verifie si le formulaire a été soumis
+        $formContact->handleRequest($request);
+        if($formContact->isSubmitted() && $formContact->isValid()){
+
+            // Etape3.1: On enregiste dans la BDD
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+        }
         return $this->render('contact/index.html.twig', [
             'formContact'=> $formContact->createView(),
         ]);
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,12 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Contact;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactType extends AbstractType
 {
