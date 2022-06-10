7083d73d794665d74441853eb91b7e1d5e381e70
Lise Rochat <liserochat@live.fr>
2022-06-10 12:21:15 +0200

    update entity

--- src/Entity/ImagesBien.php
+++ src/Entity/ImagesBien.php
@@ -4,8 +4,14 @@ namespace App\Entity;
 
 use App\Repository\ImagesBienRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: ImagesBienRepository::class)]
+/**
+ * @ORM\Entity
+ * @Vich\Uploadable
+ */
 class ImagesBien
 {
     #[ORM\Id]
@@ -14,26 +20,31 @@ class ImagesBien
     private $id;
 
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
-    private $img;
+    private $imageName;
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     * 
+     * @Vich\UploadableField(mapping="product_image", fileNameProperty="imageName", size="imageSize")
+     * 
+     * @var File|null
+     */
+    private $imageFile;
+    
+    /**
+     * @ORM\Column(type="datetime")
+     *
+     * @var \DateTimeInterface|null
+     */
+    private $updatedAt;
 
     #[ORM\ManyToOne(targetEntity: Bien::class, inversedBy: 'imagesBiens')]
     private $bien;
 
+
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getImg(): ?string
-    {
-        return $this->img;
-    }
-
-    public function setImg(?string $img): self
-    {
-        $this->img = $img;
-
-        return $this;
     }
 
     public function getBien(): ?Bien
@@ -44,6 +55,109 @@ class ImagesBien
     public function setBien(?Bien $bien): self
     {
         $this->bien = $bien;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of imageName
+     */ 
+    public function getImageName()
+    {
+        return $this->imageName;
+    }
+
+    /**
+     * Set the value of imageName
+     *
+     * @return  self
+     */ 
+    public function setImageName($imageName)
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    /**
+     * Get nOTE: This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @return  File|null
+     */ 
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * Set nOTE: This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @param  File|null  $imageFile  NOTE: This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @return  self
+     */ 
+    public function setImageFile($imageFile)
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    /**
+     * @ORM\Column(type="integer")
+     *
+     * @var int|null
+     */
+    private $imageSize;
+    
+    /**
+     * Get the value of updatedAt
+     *
+     * @return  \DateTimeInterface|null
+     */ 
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set the value of updatedAt
+     *
+     * @param  \DateTimeInterface|null  $updatedAt
+     *
+     * @return  self
+     */ 
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of imageSize
+     *
+     * @return  int|null
+     */ 
+    public function getImageSize()
+    {
+        return $this->imageSize;
+    }
+
+    /**
+     * Set the value of imageSize
+     *
+     * @param  int|null  $imageSize
+     *
+     * @return  self
+     */ 
+    public function setImageSize($imageSize)
+    {
+        $this->imageSize = $imageSize;
 
         return $this;
     }
