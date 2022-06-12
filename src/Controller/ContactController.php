d85df67252bcbfca62d2fd2724708974cd5f0508
Lise Rochat <liserochat@live.fr>
2022-06-13 00:37:21 +0200

    config form in controller and view

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,18 +2,39 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, ManagerRegistry $doctrine): Response
     {
+        $contact = new Contact();
+        $formContact = $this->createForm(ContactType::class, $contact);
+        $formContact->handleRequest($request);
+
+        if($formContact->isSubmitted() && $formContact->isValid())
+        {
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+            
+            $this->addFlash('success_contact', 'Votre message à bien été envoyé.');
+
+            return $this->redirectToRoute('app_home');
+
+        }
         
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form_title' => 'Formulaire de Contact',
+            'form_submit' => 'Envoyer',
+            'formContact' => $formContact->createView(),
         ]);
     }
 }
