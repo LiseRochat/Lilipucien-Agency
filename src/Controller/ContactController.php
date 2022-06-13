1bf116ac7ea0110bc3df457e9b84be81a800e18c
Lise Rochat <liserochat@live.fr>
2022-06-13 13:54:45 +0200

    config send mail

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,7 +15,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request, ManagerRegistry $doctrine): Response
+    public function index(Request $request, ManagerRegistry $doctrine, MailerInterface $mailer): Response
     {
         $contact = new Contact();
         $formContact = $this->createForm(ContactType::class, $contact);
@@ -27,6 +29,21 @@ class ContactController extends AbstractController
             
             $this->addFlash('success_contact', 'Votre message à bien été envoyé.');
 
+            $email = (new TemplatedEmail())
+                ->from($contact->getEmail())
+                ->to('admin@lilipucien.com')
+                //->cc('cc@example.com')
+                //->bcc('bcc@example.com')
+                //->replyTo('fabien@example.com')
+                //->priority(Email::PRIORITY_HIGH)
+                ->subject($contact->getSubject())
+                ->htmlTemplate('emails/contact.html.twig')
+                ->context([
+                    'contact' => $contact
+                ]);
+
+            $mailer->send($email);
+
             return $this->redirectToRoute('app_home');
 
         }
@@ -37,4 +54,5 @@ class ContactController extends AbstractController
             'formContact' => $formContact->createView(),
         ]);
     }
+
 }
